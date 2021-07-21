351b552b116b2244a1be585d7665cfedc0b9f94f
Scott47 <scottsilver47@gmail.com>
2021-07-21 11:31:41 -0500

    introduced two new errors

--- scripts/Shows.js
+++ scripts/Shows.js
@@ -6,7 +6,7 @@ export const Shows = () => {
     let html = "<ul>"
 
     for (const show of shows) {
-        html += `<li id="show--${show.id}">${show.name}</li>`
+        html += `<li id="show--${show.id}">${shows.name}</li>`
     }
 
     html += "</ul>"
